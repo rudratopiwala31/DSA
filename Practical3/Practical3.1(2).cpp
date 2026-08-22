04b153eec2eea32b293422abf02528d6b95d814f
rudratopiwala31 <rudratopiwala31@gmail.com>
2026-08-22 19:54:45 +0530

    Update Practical3.1(2).cpp

--- Practical3/Practical3.1(2).cpp
+++ Practical3/Practical3.1(2).cpp
@@ -1,23 +1,42 @@
-cc#include<iostream>
+#include<iostream>
 using namespace std;
+
 int main()
 {
-    int a[5]={8,5,1,3,7};
-    for(int i=0; i<4; i++)
+    int n;
+    cout<<"Enter no. of students: ";
+    cin>>n;
+
+    int a[n];
+
+    cout<<"Enter marks: ";
+    for(int i=0;i<n;i++)
     {
-        int min = i;
-        for(int j=i+1; j<5; j++)
-        {
-            if(a[j] < a[min])
-                min = j;
-        }
-        int temp = a[i];
-        a[i] = a[min];
-        a[min] = temp;
+        cin>>a[i];
     }
-    for(int i=0; i<5; i++)
+
+    for(int i=0;i<n-1;i++)
+    {
+        int min=i;
+
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]<a[min])
+            {
+                min=j;
+            }
+        }
+
+        int temp=a[i];
+        a[i]=a[min];
+        a[min]=temp;
+    }
+
+    cout<<"Sorted marks: ";
+    for(int i=0;i<n;i++)
     {
         cout<<a[i]<<" ";
     }
+
     return 0;
 }
